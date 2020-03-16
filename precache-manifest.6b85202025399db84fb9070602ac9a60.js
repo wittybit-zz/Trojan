aedaef292f4bd965f08c12c2e49f718f528791e1
wittybit <vasurana142@gmail.com>
2020-03-16 22:09:27 +0530

    Updates

---
+++ precache-manifest.6b85202025399db84fb9070602ac9a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ccbc8831f2510c0e4ebaf4400779f4",
+    "url": "/github.io/Trojan/index.html"
+  },
+  {
+    "revision": "5be6947e953c3db1bcb0",
+    "url": "/github.io/Trojan/static/css/main.af40fbd3.chunk.css"
+  },
+  {
+    "revision": "0a6ecba6bde5e3d66048",
+    "url": "/github.io/Trojan/static/js/2.540ccb95.chunk.js"
+  },
+  {
+    "revision": "5be6947e953c3db1bcb0",
+    "url": "/github.io/Trojan/static/js/main.cfb6a7f9.chunk.js"
+  },
+  {
+    "revision": "4edce41b2ba22358fb30",
+    "url": "/github.io/Trojan/static/js/runtime-main.e1874c03.js"
+  },
+  {
+    "revision": "2b3a6e157de24fd5ef6ef45273602bee",
+    "url": "/github.io/Trojan/static/media/start.2b3a6e15.svg"
+  },
+  {
+    "revision": "ddd4cf970a185940bdcd23415b6ae9dc",
+    "url": "/github.io/Trojan/static/media/water.ddd4cf97.svg"
+  }
+]);
